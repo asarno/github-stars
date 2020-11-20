244384890ba5c2bffd31e3332505e6c513df1954
Anthony Sarno <anthonysarno@Anthonys-MacBook-Pro.local>
2020-11-20 11:23:03 -0800

    Updates

---
+++ precache-manifest.cc5c1c434810e0c73d46088ce93b0b42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9b406c711a46ab826a4ffdcf0ec0ab",
+    "url": "/github-stars/index.html"
+  },
+  {
+    "revision": "f513ac2200f3438d7bd5",
+    "url": "/github-stars/static/css/main.3839dce9.chunk.css"
+  },
+  {
+    "revision": "4c690df92060d3b02fe1",
+    "url": "/github-stars/static/js/2.8607996f.chunk.js"
+  },
+  {
+    "revision": "d525fc20ced237340f5798bf946ff3a4",
+    "url": "/github-stars/static/js/2.8607996f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f513ac2200f3438d7bd5",
+    "url": "/github-stars/static/js/main.4605d78e.chunk.js"
+  },
+  {
+    "revision": "910146f874f0f100e013",
+    "url": "/github-stars/static/js/runtime-main.2c96f4a9.js"
+  }
+]);
